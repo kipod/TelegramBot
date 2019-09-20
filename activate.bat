80f1d07b32ecfa56295a6df60e94d30ac08e62c2
Nikolay Chernov <nchernov@gmail.com>
2019-09-20 09:21:42 +0300

    activate script

---
+++ activate.bat
@@ -0,0 +1,21 @@
+@echo off
+set ENV_DIR=venv
+SET ACTIVE_SCRIPT="%~dp0%ENV_DIR%\Scripts\activate.bat"
+
+if not exist "%~dp0%ENV_DIR%" (
+  echo init virtual env...
+  pushd "%~dp0"
+  python -m venv %ENV_DIR%
+  echo ::=============BOT=ENV=VARS==================>> %ACTIVE_SCRIPT%
+  echo SET BOT_TOKEN="PLESE SET VALID VALUE">> %ACTIVE_SCRIPT%
+  echo ::===========================================>> %ACTIVE_SCRIPT%
+  call %ACTIVE_SCRIPT%
+  echo upgrade pip...
+  python -m pip install --upgrade pip
+  echo install requirements
+  pip install -r requirements.txt
+  echo !!! please manualy add env vars into %~dp0.venv\Scripts\activate.bat !!! 
+) else (
+  call %ACTIVE_SCRIPT%
+)
+start "VEnv" cmd
