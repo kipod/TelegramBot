99a5f66ac7c45299ea832cfbb6f597fc33a49758
LikaKnyazeva <knyaz.angelika@gmail.com>
2019-10-02 22:01:12 +0300

    fix server close

--- tasks.py
+++ tasks.py
@@ -1,5 +1,6 @@
 import psutil
 import subprocess
+import signal
 from invoke import task
 from logger import log
 
@@ -13,14 +14,16 @@ def start(_):
         return
     log(log.INFO, "starting...")
     subprocess.Popen('start python bot.py', shell=True)
-    pass
+
+
+def get_pid():
+    with open('PID', 'r') as file:
+        return int(file.readline())
 
 
 def is_running() -> bool:
     try:
-        with open('PID', 'r') as file:
-            pid = int(file.readline())
-            return psutil.pid_exists(pid)
+        return psutil.pid_exists(get_pid())
     except FileNotFoundError:
         return False
 
@@ -31,6 +34,24 @@ def status(_):
     log(log.INFO, 'running' if is_running() else 'stopped')
 
 
+def stop_():
+    if is_running():
+        log(log.INFO, "Process was starting...")
+        pid = get_pid()
+        p = psutil.Process(pid)
+        log(log.INFO, "stopping...")
+        try:
+            p.send_signal(signal.CTRL_C_EVENT)
+        except SystemError:
+            pass
+        _, alive = psutil.wait_procs([p], timeout=2)
+        for p in alive:
+            log(log.INFO, "terminate...")
+            p.terminate()
+    else:
+        print("The process was not started")
+
+
 @task
 def stop(_):
-    log(log.INFO, "stopping...")
+    stop_()
