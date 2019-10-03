77e8844bb9ffefd5f82be425f1fba401a4f0a53f
Nikolay Chernov <nchernov@gmail.com>
2019-10-04 00:16:21 +0300

    save local photo

--- bot/parser/photo.py
+++ bot/parser/photo.py
@@ -1,5 +1,20 @@
+import os
 from .message import Message
 
+
 class Photo(Message):
+    ROOT_FOLDER = 'db'
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+    def message(self, message):
+        assert isinstance(message.photo, list)
+        for photo in message.photo:
+            file_id = photo.file_id
+            file = self.bot.get_file(file_id)
+            full_file_path = os.path.join(self.ROOT_FOLDER, file.file_path)
+            os.makedirs(os.path.dirname(full_file_path), exist_ok=True)
+            with open(full_file_path, 'wb') as f:
+                f.write(self.bot.download_file(file.file_path))
+            pass
