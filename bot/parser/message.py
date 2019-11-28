04c3194c0cb52ef5b8cbbc3174a0b596042ee59f
Volodymyr <vvl5656@gmail.com>
2019-11-28 17:17:57 +0200

    fixed checking user and file

--- bot/parser/message.py
+++ bot/parser/message.py
@@ -1,6 +1,25 @@
+from bot.users import users
+
+
 class Message(object):
     def __init__(self, bot):
         self.bot = bot
         self.name = ''
         self.surname = ''
         self.age = 0
+
+    @staticmethod
+    def check_user(user):
+        u = users[str(user.id)]
+        if not u:
+            users.add_user(uid=user.id, name=user.first_name, surname=user.last_name, username=user.username)
+
+    @staticmethod
+    def check_file(file, uid, content_type):
+        user = users[str(uid)]
+        user_file = user.get_file_by_id(file.file_id)
+        if not user_file:
+            user.add_file(file_id=file.file_id, content_type=content_type, file_path=file.file_path)
+            return False
+        else:
+            return True
--- bot/parser/photo.py
+++ bot/parser/photo.py
@@ -1,7 +1,6 @@
 import os
 from logger import log
 from .message import Message
-from bot.users import users
 from bot.consts import DB_DIR
 
 
@@ -12,12 +11,12 @@ class Photo(Message):
 
     def message(self, message):
         assert isinstance(message.photo, list)
-        self.__check_user(message.from_user)
+        self.check_user(message.from_user)
         photo = message.photo[-1]
         user_id = message.from_user.id
         file_id = photo.file_id
         file = self.bot.get_file(file_id)
-        if self.__check_file(file, user_id):
+        if self.check_file(file, user_id, message.content_type):
             self.bot.send_message(message.from_user.id, 'This file already exists')
             return
         full_file_path = os.path.join(DB_DIR, str(user_id), file.file_path)
@@ -28,19 +27,3 @@ class Photo(Message):
         self.bot.send_message(message.from_user.id,
                               'Photo Size: {}x{} Total: {} KB'.format(photo.height,
                                                                       photo.width, photo.file_size // 1024))
-
-    @staticmethod
-    def __check_user(user):
-        u = users[str(user.id)]
-        if not u:
-            users.add_user(uid=user.id, name=user.first_name, surname=user.last_name, username=user.username)
-
-    @staticmethod
-    def __check_file(file, uid):
-        user = users[str(uid)]
-        user_file = user.get_file_by_id(file.file_id)
-        if not user_file:
-            user.add_file(file_id=file.file_id, content_type='photo', file_path=file.file_path)
-            return False
-        else:
-            return True
